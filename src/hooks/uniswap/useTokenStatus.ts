a3ce7fc8150b234a58219ec816b41abf9816d070
chuangpeng <287088242@qq.com>
2026-02-25 22:37:29 +0800

    判断是原生代币，则不查询

--- src/hooks/uniswap/useTokenStatus.ts
+++ src/hooks/uniswap/useTokenStatus.ts
@@ -1,5 +1,6 @@
 import { GET_TOKEN_STATUS } from "@Mu/graphql/Queries";
 import { useTheGraphQuery } from "@Mu/hooks/graphql/useTheGraphQuery";
+import { ETHMAIN_NATIVE_TOKEN_ID } from "@Mu/types/TokenTypes";
 import type { GetTokenStatusResponse, GetTokenStatusVariables } from "@Mu/types/Uniswap";
 
 export const useTokenStatus = (tokenAddress: string) => {
@@ -9,7 +10,7 @@ export const useTokenStatus = (tokenAddress: string) => {
   >(
     GET_TOKEN_STATUS,
     { tokenAddress },
-    { enabled: !!tokenAddress }
+    { enabled: !!tokenAddress && tokenAddress !== ETHMAIN_NATIVE_TOKEN_ID }
   );
 
   return {
